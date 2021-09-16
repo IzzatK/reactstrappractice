f927e5ca0c18b8ead16dbc0a085d3570229e94c9
IzzatK <izzatkhadim@gmail.com>
2021-09-16 16:06:16 -0400

    position signincard and finish styling

--- src/SigninCard.js
+++ src/SigninCard.js
@@ -1,21 +1,24 @@
 import React, {Component} from 'react';
-import './SigninCard.css'
+import './SigininCard.css'
 
 export default class SigninCard extends Component {
 
     render() {
         return (    
-                    <div class="d-flex" style={{width:"150%"}}>
-                        <div class="row-content justify-content-center">
-                            <div id="reservationcard" class="card">
+                    <div class="d-flex">
+                        <div class="row justify-content-center">
+                            <div id="reservationcard" class="card" id="card1">
                                 <h3 class="card-header bg-warning text-white text-center">Sign in to your kloudstudio account</h3>
                                 <div class="card-body">
-                                    <div class="row mr-5 justify-content-left align-text-left">
-                                       <p id="reservecardpars ">Signin with email </p>
-                                       <br/>
-  <input class="offset-4"type="text" id="fname" name="fname"/><br/>
+                                    <div class="col-3">
+                                       <p id="reservecardpars ">With email </p>
                                        
-                                    </div>
+  <input class=""type="text" id="fname" name="fname"/><br/>
+  <p id="reservecardpars ">Password </p>
+                                       
+                                       <input class=""type="text" id="fname" name="fname"/><br/>
+    </div>
+                                       
                                     {/* <div class="row justify-content-center">
                                     
                                     <>
@@ -25,7 +28,7 @@ export default class SigninCard extends Component {
                                     </>
                                     </div> */}
                                     <div class="row justify-content-center ">
-                                    <button type="button" style={{width: "50%"}} class="btn btn-primary btn-sm">Small button</button>                                    </div>
+                                    <button type="button" style={{width: "27.7%"}} class="btn btn-primary btn-sm">Small button</button>                                    </div>
                                 </div>
                             </div>
                         </div>
