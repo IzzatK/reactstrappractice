4042180f5509ebe22d33918077553c945e205c65
IzzatK <izzatkhadim@gmail.com>
2021-09-16 15:00:40 -0400

    style topnav

--- src/App.js
+++ src/App.js
@@ -3,17 +3,23 @@ import './App.css';
 import './bootstrap.css';
 import PopupMenu from './PopupMenu'
 import SidenavMenu from './SidenavMenu';
+import TopnavMenu from './TopnavMenu';
 
 function App() {
   return (
     <div className="App">
-   
+   <div className="container" id="popupmenocontainer">
       <div id="popupmenu">
         <PopupMenu/>
       </div>
-      <div id="sidenavmenu">
-        <SidenavMenu/>
       </div>
+      {/* wrap container around the sidenavmenu to add "left sided offset" */}
+      {/* <div id="sidenavmenu">
+        <SidenavMenu/>
+      </div> */}
+    <div className="container" id="topnavmenu">
+      <TopnavMenu/>
+    </div>
       
     </div>
   );
--- src/SidenavMenu.js
+++ src/SidenavMenu.js
@@ -1,11 +1,11 @@
 import React, {Component} from 'react';
-import 'SidenavMenu.css'
+import './SidenavMenu.css'
 
 export default class SidenavMenu extends Component {
     render() {
         return (
-            <nav class ="navbar bg-dark" id="sidenavbar" >
-<ul class ="nav navbar-nav">
+            <nav class ="navbar bg-success" id="sidenavbar" >
+<ul class ="nav navbar-nav" id="sidenavbar">
 <li class ="nav-item">
 <a class ="nav-link" href="#"> Home </a>
 </li>
