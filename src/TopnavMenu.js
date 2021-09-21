f0224a80af66bf1032ac2f18aac7a89feafe3774
IzzatK <izzatkhadim@gmail.com>
2021-09-21 12:08:01 -0400

    changed navbar logo

--- src/TopnavMenu.js
+++ src/TopnavMenu.js
@@ -1,11 +1,12 @@
-import React, {Component} from 'react'
-import './TopnavMenu.css'
+import React, { Component } from "react";
+import "./TopnavMenu.css";
+import CloudPic from "./cloud.png";
 
 export default class TopnavMenu extends Component {
-    render() {
-         return (
-             <>
-             {/* <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  render() {
+    return (
+      <>
+        {/* <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <a class="navbar-brand" href="#">Navbar w/ text</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -27,26 +28,37 @@ export default class TopnavMenu extends Component {
     </span>
   </div>
 </nav> */}
-<nav class="navbar bg-dark navbar-expand-sm navbar-dark" id="topnav">
-  <div class="container" id="navcontainer">
-<a class="navbar-brand" href="#">Navbar w/ text</a>
-<h2 class="text-white">Kloudstudio</h2>
-<ul class="navbar-nav ml-auto">
-<li class="nav-item">
-<a class="nav-link" href="home">Home</a>
-</li>
-<li class="nav-item">
-<a class="nav-link" href="about">About</a>
-</li>
-<li class="nav-item">
-<a class="nav-link" href="contact">Contact us</a>
-</li>
-</ul>
-</div>
-</nav>
-
-             </>
-
-         )
-    }
+        <nav class="navbar bg-dark navbar-expand-sm navbar-dark" id="topnav">
+          <div class="container" id="navcontainer">
+            <a class="navbar-brand" href="#">
+              <img
+                src={CloudPic}
+                style={{ width: "21%", height: "auto" }}
+                class="img-thumbnail"
+                alt="cloudpic"
+              />
+            </a>
+            <h2 class="text-white">Kloudstudio</h2>
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="home">
+                  Home
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="about">
+                  About
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="contact">
+                  Contact us
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </>
+    );
+  }
 }
