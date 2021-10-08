1173fea6164d7dcb00d777745e2cbb62875bebc2
IzzatK <izzatkhadim@gmail.com>
2021-10-08 10:47:26 -0400

    converted to funcl components

--- src/PopupMenu.js
+++ src/PopupMenu.js
@@ -1,19 +1,38 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
-export default class PopupMenu extends Component {
-    render() {
-        return ( 
-                <div class="dropdown">
-                 <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                      Dropdown button
-                 </button>
-                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                 <a class="dropdown-item" href="#">Action</a>
-                 <a class="dropdown-item" href="#">Another action</a>
-                 <a class="dropdown-item" href="#">Something else here</a>
-                  </div>
-                   </div>
-      )
-    }
+const PopUpNavBar = () => {
+  return (
+    <div class="dropdown">
+      <button
+        class="btn btn-secondary dropdown-toggle"
+        type="button"
+        id="dropdownMenuButton"
+        data-toggle="dropdown"
+        aria-haspopup="true"
+        aria-expanded="false"
+      >
+        Dropdown button
+      </button>
+      <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+        <a class="dropdown-item" href="#">
+          Action
+        </a>
+        <a class="dropdown-item" href="#">
+          Another action
+        </a>
+        <a class="dropdown-item" href="#">
+          Something else here
+        </a>
+      </div>
+    </div>
+  );
+};
 
-}
+const PopUpMenu = () => {
+  return (
+    <>
+      <PopUpNavBar />
+    </>
+  );
+};
+export default PopUpMenu;
--- src/SidenavMenu.js
+++ src/SidenavMenu.js
@@ -1,25 +1,40 @@
-import React, {Component} from 'react';
-import './SidenavMenu.css'
+import React, { Component } from "react";
+import "./SidenavMenu.css";
 
-export default class SidenavMenu extends Component {
-    render() {
-        return (
-            <nav class ="navbar bg-success" id="sidenavbar" >
-<ul class ="nav navbar-nav" id="sidenavbar">
-<li class ="nav-item">
-<a class ="nav-link" href="#"> Home </a>
-</li>
-<li class ="nav-item">
-<a class ="nav-link" href="#"> Services </a>
-</li>
-<li class ="nav-item">
-<a class ="nav-link" href="#"> Contact </a>
-</li>
-<li class ="nav-item">
-<a class ="nav-link" href="#"> Blogs </a>
-</li>
-</ul>
-</nav>
-        )
-    }
-}
+const SideNavBar = () => {
+  return (
+    <nav class="navbar bg-success" id="sidenavbar">
+      <ul class="nav navbar-nav" id="sidenavbar">
+        <li class="nav-item">
+          <a class="nav-link" href="#">
+            {" "}
+            Home{" "}
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">
+            {" "}
+            Services{" "}
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">
+            {" "}
+            Contact{" "}
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">
+            {" "}
+            Blogs{" "}
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+const SidenavMenu = () => {
+  return <>{SideNavBar()}</>;
+};
+export default SideNavBar;
